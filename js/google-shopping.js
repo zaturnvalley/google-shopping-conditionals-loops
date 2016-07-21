75cba14b6032e88970c991baf0f7503338e874a4
zaturnvalley <zacharycmason@gmail.com>
2016-07-20 17:46:18 -0700

    constructed navigational for loops for all questions

--- js/google-shopping.js
+++ js/google-shopping.js
@@ -1,5 +1,57 @@
 var data = require('../products.json');
 
+var shoppingCount = 0;
 // Write your solutions below
 
-console.log(data.items);
+// console.log(data.items);
+//1
+// for (var i = 0; i < data.items.length; i++){
+// 	var item = data.items[i];
+// 	if (item.kind === 'shopping#product'){
+// 		shoppingCount++;
+// 	}
+// }
+// console.log(shoppingCount);
+
+//2
+// for (var i = 0; i < data.items.length; i++) {
+// 	var item = data.items[i];
+// 	if (item.product.inventories[0].availability === 'backorder'){
+// 		console.log(item.product.title);
+// 	}
+// }
+
+//3
+// for (var i = 0; i < data.items.length; i++) {
+// 	var item = data.items[i];
+
+// 	if (item.product.images.length > 1){
+// 		console.log(item.product.title);
+// 	}
+// }
+
+//4
+// for (var i = 0; i < data.items.length; i++) {
+// 	var item = data.items[i];
+
+// 	if (item.product.brand === 'Canon'){
+// 		console.log(item.product.title);
+// 	}
+// }
+
+//5
+// for (var i = 0; i < data.items.length; i++) {
+// 	var item = data.items[i];
+
+// 	if (item.product.author.name === 'eBay' && item.product.brand === 'Canon'){
+// 		console.log(item.product.title);
+// 	}
+// }
+
+//6
+for (var i = 0; i < data.items.length; i++) {
+	var item = data.items[i];
+
+	console.log(item.product.brand, item.product.inventories[0].price, item.product.images[0].link);
+	
+}
